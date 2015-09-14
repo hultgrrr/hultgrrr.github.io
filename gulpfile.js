86c21d515a0479f591e7cce89e5ee6cc4ba5f47f
macondele <marcus@betraktel.se>
2015-09-14 16:05:37 +0200

    Project started up

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+
+gulp.task('styles', function() {
+    gulp.src('src/styles/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css'))
+        .pipe(plugins.livereload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/styles/main.scss',['styles']);
+});
+
+gulp.task('default', ['styles', 'watch']);
+
