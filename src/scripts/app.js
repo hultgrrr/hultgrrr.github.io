33126565f5e382fe358395344a1459dcb07d0e46
macondele <marcus@betraktel.se>
2015-09-15 13:44:53 +0200

    Hiding other questions

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,18 +1,27 @@
 $(document).ready(function() {
-    var questions = [];
-    var questionsBlock = $('#quiz_questions');
+    var quiz = {};
+    quiz.questions = [];
+    quiz.quizBlock = $('#quiz_block');
+    quiz.questionsBlock = $('#quiz_questions');
+    quiz.goToQuestion = function (number) {
+        $('.quiz-question').map(function (idx) {
+            // ++ cuz of zero index
+            if (++idx != number) {
+                $(this).hide();
+            }
+        });
+    };
 
     createjs.Sound.registerSound("assets/sounds/thunder.mp3", 1);
 
     $.getJSON("questions.json", function(json) {
-        questions = json;
+        quiz.questions = json;
 
-        var readyForAction = buildDOMTree(questions);
-        console.log(readyForAction);
+        var readyForAction = buildDOMTree(quiz.questions);
 
         function buildDOMTree(questions) {
             // loop through questions and set the html up
-            var block = questionsBlock;
+            var block = quiz.questionsBlock;
             $.each(questions, function (idx, question) {
                 var node = $('<div>').addClass('quiz-question');
                 node.append($('<h1>').text(question.title));
@@ -32,8 +41,6 @@ $(document).ready(function() {
             return block;
         }
 
-        questionsBlock.fadeIn();
-
         // start quiz on user click
         $('#quiz_start').click(function () {
             createjs.Sound.play(1);
@@ -42,7 +49,8 @@ $(document).ready(function() {
                 createjs.Sound.stop(1);
 
                 // fade in the questions
-                questionsBlock.fadeIn();
+                quiz.goToQuestion(1);
+                quiz.quizBlock.fadeIn();
             });
         });
     });
